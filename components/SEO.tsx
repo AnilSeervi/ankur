4912b1f6bcdcf8b3b2805b4c4b2e6cb3704594f7
Anil Seervi <61609033+AnilSeervi@users.noreply.github.com>
2022-09-08 21:25:54 +0530

    🎉 Initial Setup

---
+++ components/SEO.tsx
@@ -0,0 +1,43 @@
+import Head from "next/head"
+
+const SEO = () => {
+	return (
+		<Head>
+			<meta
+				name="viewport"
+				content="width=device-width,initial-scale=1,minimum-scale=1,user-scalable=yes"
+			/>
+			<link rel="shortcut icon" href="ankur.jpg" type="image/svg" />
+			<title>Ankur Rawal | Zenduty</title>
+			<meta
+				name="description"
+				content="Co-Founder & CTO of Zenduty. Building Zenduty - minimizing downtime at companies and institutionalizing reliability and modern incident response best practices, one incident at a time."
+			/>
+			<meta name="robots" content="index,follow" />
+			<meta name="theme-color" content="#a033ff" />
+			<meta name="author" content="Ankur Rawal" />
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:site" content="@ankurrawal1987" />
+			<meta name="twitter:creator" content="@ankurrawal1987" />
+
+			<meta
+				property="og:description"
+				content="Co-Founder & CTO of Zenduty. Building Zenduty - minimizing downtime at companies and institutionalizing reliability and modern incident response best practices, one incident at a time."
+			/>
+			<meta
+				property="og:image"
+				content="https://ankur.pages.dev/assets/og-thumb.png"
+			/>
+			<meta property="og:image:alt" content="Ankur Rawal" />
+			<meta property="og:image:height" content="1080" />
+			<meta property="og:image:type" content="image/png" />
+			<meta property="og:image:width" content="1920" />
+			<meta property="og:site_name" content="Ankur Rawal | Zenduty" />
+			<meta property="og:title" content="Ankur Rawal" />
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content="https://ankur.pages.dev/" />
+		</Head>
+	)
+}
+
+export default SEO
